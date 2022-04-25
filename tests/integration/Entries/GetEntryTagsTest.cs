8b8b822e4c4117266aafcd7cfc3931834e314984
Jason Jia <jason.jia@laserfiche.com>
2022-04-25 10:08:53 -0400

    Add integration test for GetTagsAssignedToEntryForEachAsync

--- tests/integration/Entries/GetEntryTagsTest.cs
+++ tests/integration/Entries/GetEntryTagsTest.cs
@@ -27,5 +27,28 @@ namespace Laserfiche.Repository.Api.Client.IntegrationTest.Entries
             var response = await client.GetTagsAssignedToEntryAsync(TestConfig.RepositoryId, entryId);
             Assert.IsNotNull(response.Result?.Value);
         }
+
+        [TestMethod]
+        public async Task GetEntryTags_Paging()
+        {
+            int entryId = 1;
+            int maxMageSize = 10;
+
+            bool PagingCallback(ODataValueContextOfIListOfWTagInfo data)
+            {
+                if (data.OdataNextLink != null)
+                {
+                    Assert.AreNotEqual(0, data.Value.Count);
+                    Assert.IsTrue(data.Value.Count <= maxMageSize);
+                    return true; // If data aren't exhusted, keep asking.
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            await client.GetTagsAssignedToEntryForEachAsync(PagingCallback, TestConfig.RepositoryId, entryId, string.Format("maxpagesize={0}", maxMageSize));
+        }
     }
 }
