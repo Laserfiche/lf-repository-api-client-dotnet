90d759f46c23eaddf3cb18eee56eecf739b5c5a6
Jason Jia <jason.jia@laserfiche.com>
2022-04-25 10:01:10 -0400

    Add integration test for GetTagDefinitionsForEachAsync

--- tests/integration/TagDefinitions/GetTagDefinitionsTest.cs
+++ tests/integration/TagDefinitions/GetTagDefinitionsTest.cs
@@ -26,5 +26,27 @@ namespace Laserfiche.Repository.Api.Client.IntegrationTest.TagDefinitions
             var response = await client.GetTagDefinitionsAsync(TestConfig.RepositoryId);
             Assert.IsNotNull(response.Result?.Value);
         }
+
+        [TestMethod]
+        public async Task GetTagDefinitions_Paging()
+        {
+            int maxMageSize = 10;
+
+            bool PagingCallback(ODataValueContextOfIListOfWTagInfo data)
+            {
+                if (data.OdataNextLink != null)
+                {
+                    Assert.AreNotEqual(0, data.Value.Count);
+                    Assert.IsTrue(data.Value.Count <= maxMageSize);
+                    return true; // If data aren't exhusted, keep asking.
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            await client.GetTagDefinitionsForEachAsync(PagingCallback, TestConfig.RepositoryId, string.Format("maxpagesize={0}", maxMageSize));
+        }
     }
 }
