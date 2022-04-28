5a520c64527802ea6916138b14b59424e73fbe3a
Jason Chan <jason.chan@laserfiche.com>
2022-04-28 11:49:31 -0400

    Update client options interface

--- src/ClientOption.cs
+++ src/ClientOption.cs
@@ -5,13 +5,16 @@ using System.Threading.Tasks;
 
 namespace Laserfiche.Repository.Api.Client
 {
+    /// <summary>
+    /// Configuration parameters to create the API client.
+    /// </summary>
     public class ClientOptions : IClientOptions
     {
-        public string Domain { get; set; }
+        /// <inheritdoc/>
+        public Func<HttpRequestMessage, ILaserficheRepositoryApiClient, CancellationToken, Task<string>> BeforeSendAsync { get; set; }
+            = (request, repositoryClient, cancellationToken) => Task.FromResult((string)null);
 
-        public Func<HttpRequestMessage, ILaserficheRepositoryApiClient, CancellationToken, Task> BeforeSendAsync { get; set; }
-            = (request, repositoryClient, cancellationToken) => Task.CompletedTask;
-
+        /// <inheritdoc/>
         public Func<HttpResponseMessage, ILaserficheRepositoryApiClient, CancellationToken, Task<bool>> AfterSendAsync { get; set; }
             = (response, repositoryClient, cancellationToken) => Task.FromResult(false);
     }
