02eabf11a8be311ca338ba3fe6123b8887ad30ff
Jason Chan <jason.chan@laserfiche.com>
2022-12-13 16:35:35 -0500

    Fix documentation warning

--- src/Clients/EntriesClient.cs
+++ src/Clients/EntriesClient.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Laserfiche.Api.Client;
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading;
--- src/Clients/SearchesClient.cs
+++ src/Clients/SearchesClient.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Laserfiche.Api.Client;
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
