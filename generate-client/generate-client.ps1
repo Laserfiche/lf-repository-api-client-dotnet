803e7670c571079592f173bc6f040afebc25c686
Jason Chan <jason.chan@laserfiche.com>
2023-05-30 16:40:49 -0400

    Add workflow for generating the client

---
+++ generate-client/generate-client.ps1
@@ -0,0 +1,32 @@
+param(
+  [Parameter(Mandatory=$true)][String]$input_folder,
+  [Parameter(Mandatory=$true)][String]$output_folder,
+  [Parameter(Mandatory=$true)][String]$client_filename
+)
+
+$input_folder = $input_folder.TrimEnd("/", "\")
+$output_folder = $output_folder.TrimEnd("/", "\")
+
+# Generate the client
+nswag run "$input_folder/nswag.json" /variables:OutputFilename=$client_filename
+
+# Remove long namespaces from class names in the generated client. This makes the generated documentation look nicer.
+$client_filepath = "$input_folder/$client_filename" 
+(Get-Content $client_filepath) `
+  -replace "System\.CodeDom\.Compiler\.","" `
+  -replace "System\.Collections\.Generic\.","" `
+  -replace "System\.Globalization\.","" `
+  -replace "System\.IO\.","" `
+  -replace "System\.Linq\.","" `
+  -replace "System\.Net\.Http\.Headers\.","" `
+  -replace "System\.Net\.Http\.(?![\w\.]+;)","" `
+  -replace "System\.Runtime\.Serialization\.","" `
+  -replace "System\.Reflection\.","" `
+  -replace "System\.Text\.","" `
+  -replace "System\.Threading\.Tasks\.","" `
+  -replace "System\.Threading\.CancellationToken","CancellationToken" `
+  -replace "System\.(?![\w\.]+;)","" `
+  | Out-File $client_filepath
+
+# Move the generated client to the output folder
+Move-Item -Path $client_filepath -Destination "$output_folder/$client_filename" -Force
