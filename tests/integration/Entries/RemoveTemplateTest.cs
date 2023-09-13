56342b9f9c091bcb249b5034e00a435b6b3d890c
Jason Jia <jason.jia@laserfiche.com>
2023-09-13 11:27:16 -0400

    Update tests related to template removal API

--- tests/integration/Entries/RemoveTemplateTest.cs
+++ tests/integration/Entries/RemoveTemplateTest.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 namespace Laserfiche.Repository.Api.Client.IntegrationTest.Entries
 {
     [TestClass]
-    public class RemoveTemplateFromEntryTest : BaseTest
+    public class RemoveTemplateTest : BaseTest
     {
         Entry entry;
 
@@ -21,8 +21,8 @@ namespace Laserfiche.Repository.Api.Client.IntegrationTest.Entries
         {
             if (entry != null)
             {
-                DeleteEntryWithAuditReason body = new DeleteEntryWithAuditReason();
-                await client.EntriesClient.DeleteEntryInfoAsync(RepositoryId, entry.Id, body).ConfigureAwait(false);
+                StartDeleteEntryRequest body = new StartDeleteEntryRequest();
+                await client.EntriesClient.StartDeleteEntryAsync(RepositoryId, entry.Id, body).ConfigureAwait(false);
             }
         }
 
@@ -30,14 +30,14 @@ namespace Laserfiche.Repository.Api.Client.IntegrationTest.Entries
         public async Task RemoveTemplateFromEntry_ReturnEntry()
         {
             // Find a template definition with no required fields
-            WTemplateInfo template = null;
-            var templateDefinitionResult = await client.TemplateDefinitionsClient.GetTemplateDefinitionsAsync(RepositoryId).ConfigureAwait(false);
+            TemplateDefinition template = null;
+            var templateDefinitionResult = await client.TemplateDefinitionsClient.ListTemplateDefinitionsAsync(RepositoryId).ConfigureAwait(false);
             var templateDefinitions = templateDefinitionResult.Value;
             Assert.IsNotNull(templateDefinitions);
             Assert.IsTrue(templateDefinitions.Count > 0, "No template definitions exist in the repository.");
             foreach (var templateDefinition in templateDefinitions)
             {
-                var templateDefinitionFieldsResult = await client.TemplateDefinitionsClient.GetTemplateFieldDefinitionsAsync(RepositoryId, templateDefinition.Id).ConfigureAwait(false);
+                var templateDefinitionFieldsResult = await client.TemplateDefinitionsClient.ListTemplateFieldDefinitionsByTemplateIdAsync(RepositoryId, templateDefinition.Id).ConfigureAwait(false);
                 if (templateDefinitionFieldsResult.Value != null && templateDefinitionFieldsResult.Value.All(f => !f.IsRequired))
                 {
                     template = templateDefinition;
@@ -47,17 +47,17 @@ namespace Laserfiche.Repository.Api.Client.IntegrationTest.Entries
             Assert.IsNotNull(template, "Could not find a good template definition to assign to the entry");
 
             // Set the template on an entry
-            var request = new PutTemplateRequest()
+            var request = new SetTemplateRequest()
             {
                 TemplateName = template.Name
             };
             entry = await CreateEntry(client, "RepositoryApiClientIntegrationTest .Net RemoveTemplateFromEntry").ConfigureAwait(false);
-            var setTemplateEntryResult = await client.EntriesClient.WriteTemplateValueToEntryAsync(RepositoryId, entry.Id, request).ConfigureAwait(false);
+            var setTemplateEntryResult = await client.EntriesClient.SetTemplateAsync(RepositoryId, entry.Id, request).ConfigureAwait(false);
             Assert.IsNotNull(setTemplateEntryResult);
             Assert.AreEqual(template.Name, setTemplateEntryResult.TemplateName);
 
             // Delete the template on the entry
-            var deleteTemplateResponse = await client.EntriesClient.DeleteAssignedTemplateAsync(RepositoryId, entry.Id).ConfigureAwait(false);
+            var deleteTemplateResponse = await client.EntriesClient.RemoveTemplateAsync(RepositoryId, entry.Id).ConfigureAwait(false);
             var returnedEntry = deleteTemplateResponse;
             Assert.IsNotNull(returnedEntry);
             Assert.AreEqual(entry.Id, returnedEntry.Id);
