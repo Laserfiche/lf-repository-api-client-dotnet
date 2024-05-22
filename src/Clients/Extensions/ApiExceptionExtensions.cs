721cb7bc4d2bdc39701786fa5ef320c2b42de5e5
Paolo Argentieri <paolo.argentieri@laserfiche.com>
2024-05-22 13:34:17 -0700

    Revereted ApiExceptionExtensions

---
+++ src/Clients/Extensions/ApiExceptionExtensions.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Laserfiche.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+using Laserfiche.Api.Client;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace Laserfiche.Repository.Api.Client
+{
+    internal static class ApiExceptionExtensions
+    {
+        internal static ApiException Create(int statusCode, IReadOnlyDictionary<string, IEnumerable<string>> headers, string response, JsonSerializerSettings jsonSerializerSettings, Exception innerException)
+        {
+            return ApiException.Create(statusCode, headers, response, jsonSerializerSettings, innerException);
+        }
+
+        internal static ApiException Create(int statusCode, IReadOnlyDictionary<string, IEnumerable<string>> headers, ProblemDetails problemDetails, Exception innerException)
+        {
+            return ApiException.Create(statusCode, headers, problemDetails, innerException);
+        }
+
+        internal static ApiException Create(int statusCode, IReadOnlyDictionary<string, IEnumerable<string>> headers, Exception innerException)
+        {
+            return ApiException.Create(statusCode, headers, innerException);
+        }
+    }
+}
