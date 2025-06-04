b626f6cb892309fcc096380660d6a5722a1fcc66
alexandria.gomez <alexandria.gomez@WATSONVILLE>
2025-06-04 10:53:43 -0700

    add foreach

--- src/Clients/AuditReasonsClient.cs
+++ src/Clients/AuditReasonsClient.cs
@@ -8,13 +8,29 @@ using System.Threading.Tasks;
 
 namespace Laserfiche.Repository.Api.Client
 {
+    /// <summary>
+    /// The Laserfiche Repository Audit Reasons API client.
+    /// </summary>
     partial interface IAuditReasonsClient
     {
+        /// <summary>
+        /// Returns the audit reasons associated with the authenticated user using paging. Page results are returned to the <paramref name="callback"/>.
+        /// </summary>
+        /// <remarks>
+        /// - Related: <see cref="ListAuditReasonsAsync(ListAuditReasonsParameters, CancellationToken)">ListAuditReasonsAsync</see>
+        /// </remarks>
+        /// <param name="callback">A delegate that will be called each time new data is retrieved. Returns false to stop receiving more data; returns true to be called again if there's more data.</param>
+        /// <param name="parameters">Parameters for the request.</param>
+        /// <param name="maxPageSize">Optionally specify the maximum number of items to retrieve.</param>
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        Task ListAuditReasonsForEachAsync(Func<AuditReasonCollectionResponse, Task<bool>> callback, ListAuditReasonsParameters parameters, int? maxPageSize = null, CancellationToken cancellationToken = default);
+
         /// <summary>
         /// Returns the attribute key value pairs associated with the authenticated user using a nextlink.
         /// </summary>
         /// <remarks>
-        /// - Related: <see cref="ListAuditReasonsAsync(ListAuditReasonsParameters, CancellationToken)">ListAttributesAsync</see>
+        /// - Related: <see cref="ListAuditReasonsAsync(ListAuditReasonsParameters, CancellationToken)">ListAuditReasonsAsync</see>
         /// </remarks>
         /// <param name="nextLink">A url that allows retrieving the next subset of the requested collection.</param>
         /// <param name="maxPageSize">Optionally specify the maximum number of items to retrieve.</param>
