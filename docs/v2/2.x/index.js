eae679700003244e2311e723a329ff2e9a6a606c
alexgomezlf <alexgomezlf@users.noreply.github.com>
2025-07-11 23:08:31 +0000

    Automated documentation update for v2 2.x by action 16231285768

--- docs/v2/2.x/index.js
+++ docs/v2/2.x/index.js
@@ -1,11 +1,11 @@
 var index =
 [
-    [ "Documentation", "index.html#autotoc_md43", null ],
-    [ "Changelog", "index.html#autotoc_md44", null ],
-    [ "How to contribute", "index.html#autotoc_md45", [
-      [ "Generate the repository client", "index.html#autotoc_md46", null ],
-      [ "Build, test, and package", "index.html#autotoc_md47", [
-        [ "Branches", "index.html#autotoc_md48", null ]
+    [ "Documentation", "index.html#autotoc_md45", null ],
+    [ "Changelog", "index.html#autotoc_md46", null ],
+    [ "How to contribute", "index.html#autotoc_md47", [
+      [ "Generate the repository client", "index.html#autotoc_md48", null ],
+      [ "Build, test, and package", "index.html#autotoc_md49", [
+        [ "Branches", "index.html#autotoc_md50", null ]
       ] ]
     ] ]
 ];
