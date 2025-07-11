9ac0e1244fe3faafe14fbfb6dbfb50f1ab1908d9
alexgomezlf <alexgomezlf@users.noreply.github.com>
2025-07-11 14:36:48 +0000

    Automated documentation update for v1 1.x by action 16222388535

--- docs/v1/1.x/index.js
+++ docs/v1/1.x/index.js
@@ -1,11 +1,11 @@
 var index =
 [
-    [ "Documentation", "index.html#autotoc_md29", null ],
-    [ "Changelog", "index.html#autotoc_md30", null ],
-    [ "How to contribute", "index.html#autotoc_md31", [
-      [ "Generate the repository client", "index.html#autotoc_md32", null ],
-      [ "Build, test, and package", "index.html#autotoc_md33", [
-        [ "Branches", "index.html#autotoc_md34", null ]
+    [ "Documentation", "index.html#autotoc_md31", null ],
+    [ "Changelog", "index.html#autotoc_md32", null ],
+    [ "How to contribute", "index.html#autotoc_md33", [
+      [ "Generate the repository client", "index.html#autotoc_md34", null ],
+      [ "Build, test, and package", "index.html#autotoc_md35", [
+        [ "Branches", "index.html#autotoc_md36", null ]
       ] ]
     ] ]
 ];
---
+++ docs/v1/1.x/search/all_19.js
@@ -0,0 +1,21 @@
+var searchData=
+[
+  ['waitforsearchasync_0',['waitforsearchasync',['../interface_laserfiche_1_1_repository_1_1_api_1_1_client_1_1_i_searches_client.html#ae163a92184acaf860a632890e91a3ba3',1,'Laserfiche.Repository.Api.Client.ISearchesClient.WaitForSearchAsync()'],['../class_laserfiche_1_1_repository_1_1_api_1_1_client_1_1_searches_client.html#a3aa88eae8ee19d69440d6bbad93fe392',1,'Laserfiche.Repository.Api.Client.SearchesClient.WaitForSearchAsync()']]],
+  ['waitfortaskasync_1',['waitfortaskasync',['../class_laserfiche_1_1_repository_1_1_api_1_1_client_1_1_tasks_client.html#a0b36c9a4a95cad941590064586535a00',1,'Laserfiche.Repository.Api.Client.TasksClient.WaitForTaskAsync()'],['../interface_laserfiche_1_1_repository_1_1_api_1_1_client_1_1_i_tasks_client.html#ad6108a1c5e8078888c5f44f8718057e8',1,'Laserfiche.Repository.Api.Client.ITasksClient.WaitForTaskAsync()']]],
+  ['warning_2',['2. Warning',['../md__c_o_d_e___o_f___c_o_n_d_u_c_t.html#autotoc_md26',1,'']]],
+  ['watermark_3',['watermark',['../class_laserfiche_1_1_repository_1_1_api_1_1_client_1_1_watermark.html',1,'Watermark'],['../class_laserfiche_1_1_repository_1_1_api_1_1_client_1_1_w_tag_info.html#ad0ddfcea46997c63569fb34002d80e98',1,'Laserfiche.Repository.Api.Client.WTagInfo.Watermark']]],
+  ['watermarkintensity_4',['WatermarkIntensity',['../class_laserfiche_1_1_repository_1_1_api_1_1_client_1_1_watermark.html#aae0873d1154f70eece6c276a1ed21e05',1,'Laserfiche::Repository::Api::Client::Watermark']]],
+  ['watermarkposition_5',['WatermarkPosition',['../class_laserfiche_1_1_repository_1_1_api_1_1_client_1_1_watermark.html#a2535fb9ac7c9d742889f8904ebf54d53',1,'Laserfiche::Repository::Api::Client::Watermark']]],
+  ['watermarkrotationangle_6',['WatermarkRotationAngle',['../class_laserfiche_1_1_repository_1_1_api_1_1_client_1_1_watermark.html#a6b28d7ae3d0cdea65e0c7ab0c8fe7bed',1,'Laserfiche::Repository::Api::Client::Watermark']]],
+  ['watermarktext_7',['WatermarkText',['../class_laserfiche_1_1_repository_1_1_api_1_1_client_1_1_watermark.html#ad24418fb0a0108dd389a98e50faf8b99',1,'Laserfiche::Repository::Api::Client::Watermark']]],
+  ['watermarktextsize_8',['WatermarkTextSize',['../class_laserfiche_1_1_repository_1_1_api_1_1_client_1_1_watermark.html#a0bdf2600b9bd9076707d09bd7e261ab8',1,'Laserfiche::Repository::Api::Client::Watermark']]],
+  ['wauditreason_9',['WAuditReason',['../class_laserfiche_1_1_repository_1_1_api_1_1_client_1_1_w_audit_reason.html',1,'Laserfiche::Repository::Api::Client']]],
+  ['webclienturl_10',['WebclientUrl',['../class_laserfiche_1_1_repository_1_1_api_1_1_client_1_1_repository_info.html#ada0520aadf38a0d779d79f034ef8fbac',1,'Laserfiche::Repository::Api::Client::RepositoryInfo']]],
+  ['wentrylinkinfo_11',['WEntryLinkInfo',['../class_laserfiche_1_1_repository_1_1_api_1_1_client_1_1_w_entry_link_info.html',1,'Laserfiche::Repository::Api::Client']]],
+  ['wfieldformat_12',['WFieldFormat',['../namespace_laserfiche_1_1_repository_1_1_api_1_1_client.html#ab7288a8188a0f4d98bcf00f49bfdf135',1,'Laserfiche::Repository::Api::Client']]],
+  ['wfieldinfo_13',['WFieldInfo',['../class_laserfiche_1_1_repository_1_1_api_1_1_client_1_1_w_field_info.html',1,'Laserfiche::Repository::Api::Client']]],
+  ['wfieldtype_14',['WFieldType',['../namespace_laserfiche_1_1_repository_1_1_api_1_1_client.html#a39e69fa2cf118e347e2f2d48abb88105',1,'Laserfiche::Repository::Api::Client']]],
+  ['writetemplatevaluetoentryasync_15',['writetemplatevaluetoentryasync',['../interface_laserfiche_1_1_repository_1_1_api_1_1_client_1_1_i_entries_client.html#aadecaf7afceb1239d7f88bd8dbc0864e',1,'Laserfiche.Repository.Api.Client.IEntriesClient.WriteTemplateValueToEntryAsync()'],['../class_laserfiche_1_1_repository_1_1_api_1_1_client_1_1_entries_client.html#ac019c1bf63c7d73c2db08644a851e30e',1,'Laserfiche.Repository.Api.Client.EntriesClient.WriteTemplateValueToEntryAsync()']]],
+  ['wtaginfo_16',['WTagInfo',['../class_laserfiche_1_1_repository_1_1_api_1_1_client_1_1_w_tag_info.html',1,'Laserfiche::Repository::Api::Client']]],
+  ['wtemplateinfo_17',['WTemplateInfo',['../class_laserfiche_1_1_repository_1_1_api_1_1_client_1_1_w_template_info.html',1,'Laserfiche::Repository::Api::Client']]]
+];
